c507bb8c7362805ffa545c15c1d25019326352f5
林福军 <linfujun@gridsum.com>
2018-03-31 19:06:24 +0800

    用户curd修改

--- dangjian-admin/dangjian-admin-web/src/main/java/cn/dlbdata/dangjian/admin/web/VO/LoginVO.java
+++ dangjian-admin/dangjian-admin-web/src/main/java/cn/dlbdata/dangjian/admin/web/VO/LoginVO.java
@@ -7,4 +7,28 @@ public class LoginVO {
     private String ptoken;
     private Integer userId;
     private Integer roleId;
+
+    public String getPtoken() {
+        return ptoken;
+    }
+
+    public void setPtoken(String ptoken) {
+        this.ptoken = ptoken;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
 }
