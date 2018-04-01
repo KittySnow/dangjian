5e668fba22f1c6d59ff078f3b155ef1460093a6c
KittySnow <158086780@qq.com>
2018-04-01 09:49:59 +0800

    增加新文件

---
+++ dangjian-admin/dangjian-admin-dao/src/main/java/cn/dlbdata/dangjian/admin/dao/mapper/PActiveDao.java
@@ -0,0 +1,35 @@
+package cn.dlbdata.dangjian.admin.dao.mapper;
+
+import cn.dlbdata.dangjian.admin.dao.model.PActive;
+import cn.dlbdata.dangjian.admin.dao.model.PActiveExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+
+
+@Mapper
+public interface PActiveDao {
+    long countByExample(PActiveExample example);
+
+    int deleteByExample(PActiveExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PActive record);
+
+    int insertSelective(PActive record);
+
+    List<PActive> selectByExample(PActiveExample example);
+
+    PActive selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") PActive record, @Param("example") PActiveExample example);
+
+    int updateByExample(@Param("record") PActive record, @Param("example") PActiveExample example);
+
+    int updateByPrimaryKeySelective(PActive record);
+
+    int updateByPrimaryKey(PActive record);
+}
