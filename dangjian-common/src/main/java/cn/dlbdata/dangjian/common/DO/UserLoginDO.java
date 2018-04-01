549eec83de01f896563e73dba00dcac23d10ae07
KittySnow <158086780@qq.com>
2018-04-01 10:03:48 +0800

    增加set get的方式！！！

--- dangjian-common/src/main/java/cn/dlbdata/dangjian/common/DO/UserLoginDO.java
+++ dangjian-common/src/main/java/cn/dlbdata/dangjian/common/DO/UserLoginDO.java
@@ -6,4 +6,20 @@ import lombok.Data;
 public class UserLoginDO {
     private String name;
     private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
